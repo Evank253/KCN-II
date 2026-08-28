069d88e890c6b238e1a1af24c67550ef89410202
Evank253 <evank253@users.noreply.github.com>
2026-08-28 06:21:58 +0000

    Remove stray route copies from src root

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { KcnConsole } from "@/components/kcn/console";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <KcnConsole />;
-}
